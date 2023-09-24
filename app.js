edfe3d0084e71b3164bf2b72326354f0a5a77869
iamcelestino <trcelestino488@gmail.com>
2023-09-23 21:35:54 -0700

    javascript completed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+const adviceText = document.querySelector(".advice__text");
+const adviceGenegartorBtn = document.querySelector(".generate__btn");
+const adviceId = document.querySelector(".adviceId")
 
 const getRandomAdvice = async () => {
     const advice = await fetch("https://api.adviceslip.com/advice");
@@ -6,6 +8,13 @@ const getRandomAdvice = async () => {
     return response;
 }
 
-getRandomAdvice()
-    .then(advice => console.log(advice))
-    .catch(error => console.log(error));
+adviceGenegartorBtn.addEventListener("click", () => {
+    getRandomAdvice()
+    .then(response => {
+        adviceId.textContent = response.slip.id
+        adviceText.textContent = response.slip.advice
+    }
+    ).catch(err => console.log(err));
+});
+
+
